b6e23309fb4129dcce6604ae015f3f8c9238d788
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-05 12:38:23 +0300

    completed delete item

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Task({ category, text }) {
+function Task({ category, text, handleDelete }) {
+
   return (
     <div className="task">
       <div className="label">{category}</div>
       <div className="text">{text}</div>
-      <button className="delete">X</button>
+      <button className="delete" onClick={handleDelete}>X</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "./Task";
 
 function TaskList({ tasks }) {
+
+  const [allTasks, setAllTasks] = useState(tasks)
+
+  function deleteTask(event) {
+    const taskContainer = event.target.parentNode
+    const itemName = taskContainer.querySelector(".text").textContent
+    const updatedItems = allTasks.filter((task) => task.text !== itemName)
+    setAllTasks(updatedItems)
+  }
 
   return (
     <>
       <div className="tasks">
         {/* display a list of tasks using Task component */}
         <ul>
-          {tasks.map((task) => {
+          {allTasks.map((task) => {
             return (
-              <Task key={task.text} category={task.category} text={task.text} />
+              <Task key={task.text} handleDelete={deleteTask} category={task.category} text={task.text} />
             )
           })}
         </ul>
