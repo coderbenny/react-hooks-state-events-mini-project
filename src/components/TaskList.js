4fd46b5b408c79dfc06a15696611cac8b2e7c44d
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-05 09:18:18 +0300

    Completed setting up the tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+    <>
+      <div className="tasks">
+        {/* display a list of tasks using Task component */}
+        <ul>
+          {tasks.map((task) => {
+            return (
+              <Task category={task.category} text={task.text} />
+            )
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
 
